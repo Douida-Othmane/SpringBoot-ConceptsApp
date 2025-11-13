995c973d1d9b77b03077d4f4cf5195c06c670f6d
Douida-Othmane <douidaothmane@gmail.com>
2025-11-13 16:59:23 +0100

    Start the application

---
+++ src/test/java/org/oth/apppractice/AppPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oth.apppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
