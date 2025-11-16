e28d3c1a4c8ebcdf9fccfebdb521f9a10a12b320
Douida-Othmane <douidaothmane@gmail.com>
2025-11-16 20:55:58 +0100

    Add JPA relation and fetch types

---
+++ src/main/java/org/oth/apppractice/Address.java
@@ -0,0 +1,20 @@
+package org.oth.apppractice;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
