842fc0169b5360e815d505166e713ad964d3e806
Douida-Othmane <douidaothmane@gmail.com>
2025-11-23 18:53:04 +0100

    impliments UserDetailsService method

--- src/main/java/org/oth/apppractice/UserService.java
+++ src/main/java/org/oth/apppractice/UserService.java
@@ -1,27 +1,26 @@
 package org.oth.apppractice;
 
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.oth.apppractice.Entity.User;
 import org.oth.apppractice.DTO.UserDTO;
 import org.oth.apppractice.mapper.UserMapper;
 import org.oth.apppractice.Exception.BusinessException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 
 @Service
-public class UserService {
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    @Autowired
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-    }
 
     public UserDTO findById(Long id) {
         User user = userRepository.findById(id).orElse(null);
@@ -80,6 +79,14 @@ public class UserService {
         }
 
         user.setEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() ->
+                    new UsernameNotFoundException(
+                            String.format("user with email %s not found", email)));
     }
 }
 
